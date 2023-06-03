4582cd6ad6b90c2c4ea46cf716964435529e8362
Matt Rodrigues <68609506+Matt-Rodrigues-EC@users.noreply.github.com>
2023-06-03 17:41:49 -0300

    delete component1 e add Header

---
+++ anime-flex/src/components/Header.vue
@@ -0,0 +1,43 @@
+<script >
+export default {
+    name: 'Header',
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="header">
+        <a>Home</a>
+        <a>Entrar</a>
+        <a>Cadastrar-se</a> 
+    </div>
+</template>
+
+<style>
+
+.header {
+    display: flex;
+    width: 100%;
+    height: 70px;
+    background-color: #454545;
+    position: fixed;
+    top: 0;
+    left: 0;
+    gap: 15px;
+    justify-content: space-evenly;
+    align-items: center;
+}
+
+input{
+    width: 250px;
+    height: 25px;
+    padding-left: 15px;
+    border: none;
+    border-radius: 15px;
+    box-sizing: border-box;
+}
+
+</style>
